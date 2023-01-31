44e5cc226c6c99df9d26adda7d447680349a80fe
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-01-31 16:21:30 +0100

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.db = router.db;
 
+server.use(cors());
 server.use(auth);
 server.use(middlewares);
 server.use(router);
